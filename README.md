4833879f397ed5fcdb155f54e22356f8ad16cde4
sukrit007 <sukrit007@gmail.com>
2015-08-30 11:41:05 -0700

    Python flask generator (microservice) for deploying in totem

--- README.md
+++ README.md
@@ -3,14 +3,52 @@ Python flask  app generator that is integrated with docker.
 - Creates bask flask app skeleton
 - Pushes the created skeleton to git origin
 
+
+## Running Generator (Docker)
+```
+sudo docker run --rm  \
+    -e GIT_USER="$GIT_USER" \
+    -e GIT_USER_EMAIL="$GIT_USER_EMAIL" \
+    -e GIT_OWNER="$GIT_OWNER" \
+    -e GIT_REPO="$GIT_REPO" \
+    -e APP_NAME="$APP_NAME" \
+    -e GITHUB_USERNAME="$GITHUB_USERNAME" \
+    -e GITHUB_TOKEN="$GITHUB_TOKEN" \
+    -e SCM_TYPE="$SCM_TYPE" \
+    -e ORCHESTRATOR_HOOK_URL="$ORCHESTRATOR_HOOK_URL" \
+    -e IMAGE_FACTORY_HOOK_URL="$IMAGE_FACTORY_HOOK_URL" \
+    -e HOOK_SECRET="$HOOK_SECRET" \
+    totem/python-flask-generator
+```
+
 ## Env variables
 
-### GIT_REPO_URL
-Target repository (git) URL.
+###  GIT_USER
+User to be used for git commits. (Required)
 
-### GIT_BRANCH
-Target git branch. Defaults to master.  If branch does not exist a new one will be created
+###  GIT_USER_EMAIL
+Email to be used for git commits. (Required)
 
-### GIT_SSH_KEY
-SSH Key for accessing git repository.
+###  GIT_OWNER
+Git repository owner. (Required)
 
+###  GIT_REPO
+Git repository name. (Required)
+
+###  APP_NAME
+Application. (Required)
+
+### SCM_TYPE
+Type of SCM (e.g: github) (Required)
+
+### GITHUB_USERNAME
+Github username (Required for github)
+
+### ORCHESTRATOR_HOOK_URL
+Callback hook url for orchestrator. (Required)
+
+### IMAGE_FACTORY_HOOK_URL
+Callback hook url for image-factory. (Required)
+
+### HOOK_SECRET
+Hook secret to be sed. (Required)
--- setup.sh
+++ setup.sh
@@ -1,25 +1,96 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-if [ -z "$GIT_REPO_URL" ]; then
-    echo "Error: GIT_REPO_URL was not initialized."
+if [ -z "$GIT_USER" ]; then
+    echo "Error: GIT_USER was not initialized."
     exit 1
 fi
 
-if [ -z "$GIT_SSH_KEY" ]; then
-    echo "Error: GIT_SSH_KEY was not initialized."
-    # exit 1
+if [ -z "$GIT_USER_EMAIL" ]; then
+    echo "Error: GIT_USER_EMAIL was not initialized."
+    exit 1
 fi
 
-GIT_REPO_WITH_EXT="$(basename $GIT_REPO_URL)"
-GIT_REPO="${GIT_REPO_WITH_EXT%.*}"
-GIT_BRANCH="${GIT_BRANCH:-master}"
-APP_NAME="${APP_NAME:-myapp}"
+if [ -z "$GIT_OWNER" ]; then
+    echo "Error: GIT_OWNER was not initialized."
+    exit 1
+fi
+
+if [ -z "$GIT_REPO" ]; then
+    echo "Error: GIT_REPO was not initialized."
+    exit 1
+fi
+
+if [ -z "$APP_NAME" ]; then
+    echo "Error: APP_NAME was not initialized."
+    exit 1
+fi
+
+if [ -z "$SCM_TYPE" ]; then
+    echo "Error: SCM_TYPE was not initialized."
+    exit 1
+elif [ "$SCM_TYPE" = "github" ]; then
+    if [ -z "$GITHUB_USERNAME" ]; then
+        echo "Error: GITHUB_USERNAME was not initialized."
+        exit 1
+    fi
+    if [ -z "$GITHUB_TOKEN" ]; then
+        echo "Error: GITHUB_TOKEN was not initialized."
+        exit 1
+    fi
+    export GIT_USERNAME="${GIT_USERNAME:-$GITHUB_TOKEN}"
+    export GIT_PASSWORD="${GIT_PASSWORD:-}"
+    export GIT_REPO_URL="https://github.com/$GIT_OWNER/$GIT_REPO.git"
+else
+    if [ -z "$GIT_REPO_URL" ]; then
+        echo "Error: GIT_REPO_URL was not initialized."
+        exit 1
+    fi
+    if [ -z "$GIT_USERNAME" ]; then
+        echo "Error: GIT_USERNAME was not initialized."
+        exit 1
+    fi
+    if [ -z "$GIT_PASSWORD" ]; then
+        echo "Error: GIT_PASSWORD was not initialized."
+        exit 1
+    fi
+fi
+
 APP_PORT="${APP_PORT:-8080}"
 PYTHON_MAJOR_VERSION="${PYTHON_MAJOR_VERSION:-2}"
 
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+function git_setup {
+    git config --global user.name "$GIT_USER"
+    git config --global user.email "$GIT_USER_EMAIL"
+    git config --global credential.helper cache
+    git config --global credential.$(dirname $(dirname $GIT_REPO_URL)).username "$GIT_USERNAME"
+    git config --global core.askpass "$(get_abs_filename gitpass.sh)"
+}
+
+function hook_setup {
+    if [ "${SCM_TYPE}" = "github" ]; then
+        if [ ! -z "${ORCHESTRATOR_HOOK_URL}" ]; then
+            curl -u "$GITHUB_USERNAME:$GITHUB_TOKEN" -XPOST -d \
+                "{\"name\": \"web\", \"active\": \"true\", \"events\": [\"push\", \"create\", \"delete\"], \
+                \"config\": {\"content_type\": \"json\", \"url\": \"$ORCHESTRATOR_HOOK_URL\",\"secret\": \"$HOOK_SECRET\"}  }" \
+                 https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/hooks
+        fi
+        if [ ! -z "${IMAGE_FACTORY_HOOK_URL}" ]; then
+            curl -u "$GITHUB_USERNAME:$GITHUB_TOKEN" -XPOST -d \
+                "{\"name\": \"web\", \"active\": \"true\", \"events\": [\"push\"], \
+                \"config\": {\"content_type\": \"json\", \"url\": \"$IMAGE_FACTORY_HOOK_URL\",\"secret\": \"$HOOK_SECRET\"}  }" \
+                 https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/hooks
+        fi
+    fi
+}
+
+
 function init_app {
-    cp -r  skeleton/* $GIT_REPO
-    cd $GIT_REPO
+    cp -r  ../skeleton/* .
 
     if [ "$APP_NAME" != "myapp" ]; then
         mv myapp $APP_NAME
@@ -29,34 +100,47 @@ function init_app {
     if [ "$APP_PORT" != "8080" ]; then
         sed -i "s/EXPOSE 8080/EXPOSE $APP_PORT/g" Dockerfile
         sed -i "s/port=8080/port=$APP_PORT/g" server.py
+        sed -i "s/8080/$APP_PORT/g" totem.yml
     fi
 
     if [ "$PYTHON_MAJOR_VERSION" != "2" ]; then
-        sed -i "s/ENV PYTHON_MAJOR_VERSION 2/ENV PYTHON_MAJOR_VERSION $PYTHON_MAJOR_VERSION/g" Dockerfile
+        sed -i \
+            -e "s/2.7-trusty-.*/3.4-trusty-b4/g" \
+            -e "s/pip /pip3 /g" Dockerfile
     fi
     
 }
 
-function git_init() {
+function git_init {
     rm -rf $GIT_REPO
     git clone "$GIT_REPO_URL"
-    if git branch | grep $GIT_BRANCH; then
-       git checkout $GIT_BRANCH
+    cd $GIT_REPO
+    if git branch | grep 'master'; then
+       git checkout master
     else
-        git checkout -b $GIT_BRANCH
+        git checkout -b master
     fi
 }
 
-function git_push() {
+function git_push {
     echo "git push"
     git add . -A
     git commit . -m 'Totem Seed application for python'
-    git push origin $GIT_BRANCH
+    git push origin master
+
+    if git branch | grep 'develop'; then
+       git checkout develop
+       git merge master
+    else
+        git checkout -b develop
+    fi
+    git push origin develop
+
 }
 
-
+git_setup
+hook_setup
 git_init
-mkdir -p $GIT_REPO
 init_app
 git_push
 
